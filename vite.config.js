c6ec37422b7ce0d1300b5a2bbd2b9e405d907359
Gilles Hemmerle <gilles.h@iabsis.com>
2026-02-27 12:08:21 +0100

    Replaced cra (deprecated) with vite to fix all the security issues

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    open: true,
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: ['legacy-js-api', 'import', 'global-builtin', 'color-functions', 'if-function', 'abs-percent'],
+      },
+    },
+  },
+});
